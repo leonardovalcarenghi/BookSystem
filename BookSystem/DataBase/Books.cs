7d86e0968a916568e15abb709b6c11cc0b9164af
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2020-09-23 20:57:17 -0300

    Buscar livros no banco de dados

---
+++ BookSystem/DataBase/Books.cs
@@ -0,0 +1,72 @@
+﻿using BookSystem.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSystem.DataBase
+{
+    public class Books
+    {
+        public static BookDTO Get(int id)
+        {
+            BookDTO book = null;
+            SqlConnection ConnectSQL = new SqlConnection(_DataBase.ConnectionString);
+            string SQL = "SELECT * FROM Books WHERE BookID = @BookID";
+
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand(SQL, ConnectSQL);
+                sqlCommand.CommandType = System.Data.CommandType.Text;
+                ConnectSQL.Open();
+                sqlCommand.Parameters.Add(new SqlParameter("@BookID", id));
+
+                SqlDataReader readDataBase = sqlCommand.ExecuteReader();
+                while (readDataBase.Read())
+                {
+                    book = new BookDTO();
+                    book.Id = id;
+                    book.Name = readDataBase["Name"].ToString();
+                    book.Author = readDataBase["Author"].ToString();
+                    book.ImageURL = readDataBase["ImageURL"].ToString();
+                }
+                return book;
+            }
+            catch (SqlException sqlEx) { throw sqlEx; }
+            finally { ConnectSQL.Close(); }
+        }
+
+
+        public static List<BookDTO> GetAll()
+        {
+            List<BookDTO> listOfBooks = null;
+            SqlConnection ConnectSQL = new SqlConnection(_DataBase.ConnectionString);
+            string SQL = "SELECT * FROM Books";
+
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand(SQL, ConnectSQL);
+                sqlCommand.CommandType = System.Data.CommandType.Text;
+                ConnectSQL.Open();
+           
+                SqlDataReader readDataBase = sqlCommand.ExecuteReader();
+                while (readDataBase.Read())
+                {
+                    if (listOfBooks == null) { listOfBooks = new List<BookDTO>(); }
+                    BookDTO book = new BookDTO();
+                    book.Id = Convert.ToInt32(readDataBase["BookID"].ToString());
+                    book.Name = readDataBase["Name"].ToString();
+                    book.Author = readDataBase["Author"].ToString();
+                    book.ImageURL = readDataBase["ImageURL"].ToString();
+                    listOfBooks.Add(book);
+                }
+                return listOfBooks;
+            }
+            catch (SqlException sqlEx) { throw sqlEx; }
+            finally { ConnectSQL.Close(); }
+        }
+
+    }
+}
---
+++ BookSystem/DataBase/_DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSystem.DataBase
+{
+    public static class _DataBase
+    {
+        public static string ConnectionString { get { return @"Data Source=localhost\SQLEXPRESS;Initial Catalog=BookSystem;Integrated Security=true;"; } }
+    }
+}
