829d264232a6d89c8cdd94017b5c6fe4de13aed8
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2020-09-23 21:12:58 -0300

    arquivo de requisições

---
+++ BookSystem/Front/Content/js/requestXHR.js
@@ -0,0 +1,19 @@
+﻿
+
+const Request = (method = 'GET', api = '', parameters = {}) => {
+
+    var callBackOK = null, callbackError = null
+    this.Success = (callback) => { callBackOK = callback; return this; }
+    this.Error = (callback) => { callbackError = callback; return this; }
+
+
+
+    var requestXHR = new XMLHttpRequest();
+
+
+    requestXHR.open(method, api, true);
+    requestXHR.setRequestHeader('Content-type', 'application/json');
+    requestXHR.send(parameters == null ? null : JSON.stringify(parameters));
+
+    return this;
+}
