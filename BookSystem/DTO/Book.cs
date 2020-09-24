c983fa948cceb37bba06d543174cd4a04bc931c7
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2020-09-23 21:29:08 -0300

    Mais informações do livro

--- BookSystem/DTO/Book.cs
+++ BookSystem/DTO/Book.cs
@@ -26,5 +26,36 @@ namespace BookSystem.DTO
         /// URL da Imagem de capa do livro
         /// </summary>
         public string ImageURL { get; set; }
+
+        /// <summary>
+        /// Descriçao do livro.
+        /// </summary>
+        public string Description { get; set; }
+
+
+        /// <summary>
+        /// Editora.
+        /// </summary>
+        public string Publisher { get; set; }
+
+        /// <summary>
+        /// Categoria
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Ano de lançamento
+        /// </summary>
+        public string Year { get; set; }
+
+        /// <summary>
+        /// Número de Páginas
+        /// </summary>
+        public int Pages { get; set; }
+
+
+
+
+
     }
 }
