74005b9517e8209ea5ab0d087f05c8c47b0a44f1
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2020-09-25 13:20:36 -0300

    Buscar livros em ordem alfabetica

--- BookSystem/DataBase/Resources/Books/GetAll.sql
+++ BookSystem/DataBase/Resources/Books/GetAll.sql
@@ -1,6 +1,6 @@
 
 -- Buscar todos os livros (sem filtro):
-	IF @Search IS NULL	BEGIN	SELECT B.*, R.Status, R.UserID AS [RentedBy] FROM Books AS B LEFT Join RentedBooks AS R ON B.BookID = R.BookID	END
+	IF @Search IS NULL	BEGIN	SELECT B.*, R.Status, R.UserID AS [RentedBy] FROM Books AS B LEFT Join RentedBooks AS R ON B.BookID = R.BookID ORDER BY B.Name ASC	END
 	ELSE
 -- Buscar livros com filtro:
-	BEGIN SELECT B.*, R.Status, R.UserID AS [RentedBy] FROM Books AS B LEFT Join RentedBooks AS R ON B.BookID = R.BookID WHERE B.Name LIKE '%' + @Search +'%' END
+	BEGIN SELECT B.*, R.Status, R.UserID AS [RentedBy] FROM Books AS B LEFT Join RentedBooks AS R ON B.BookID = R.BookID WHERE B.Name LIKE '%' + @Search +'%' ORDER BY B.Name ASC END
